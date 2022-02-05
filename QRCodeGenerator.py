773a9ece2348f83f4bd01cc8385d739811f7015e
Pawel Dabek <pawlodabek17@gmail.com>
2022-02-05 13:44:03 +0100

    Finished project

---
+++ QRCodeGenerator.py
@@ -0,0 +1,17 @@
+from ensurepip import version
+import qrcode
+import image
+
+qr = qrcode.QRCode(
+    version=15,
+    box_size = 10,
+    border = 5
+)
+
+data ="https://github.com/P4ablo"
+
+qr.add_data(data)
+qr.make(fit = True)
+
+img = qr.make_image(fill = "black", back_color = "white")
+# img.save("test.png")
